4e93777eb2df5eff1a6a9adce80835106164b272
Mari-Brauer <111579213+Mari-Brauer@users.noreply.github.com>
2023-08-09 11:43:25 -0300

    Add files via upload

---
+++ Diagrama de classe em prática.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula_Poa_Mari
+{
+    class Jogador : Program
+    {
+        string nome;
+        int idade, drible, velocidade, chute, força;
+
+        public void correr()
+        {
+            int aceleração = força + velocidade;
+        }
+        public int chutar()
+        {
+            int potenciachute = força * chute;
+            return potenciachute;
+        }
+        public double toque()
+        {
+            double forcatoque = chute / 3 + força / 3;
+            return forcatoque;
+        }
+        public double potenciacarrinho()
+        {
+            double carrinho = (velocidade * velocidade) + força / 2;
+            return carrinho;
+        }
+        public int eficaciadrible()
+        {
+            int eficienciadrible = drible + (velocidade * velocidade);
+            return eficienciadrible;
+        }
+    }
+}
